4e1298d7f0ad4ded86f158f01cc2bb1b3e81fec8
Constantine Peros <89947281+cperos@users.noreply.github.com>
2021-10-23 22:13:01 -1000

    Update StuffItem.jsx
    trash button

--- app/imports/ui/components/StuffItem.jsx
+++ app/imports/ui/components/StuffItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Icon, Button } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
@@ -13,6 +13,11 @@ class StuffItem extends React.Component {
         <Table.Cell>{this.props.stuff.condition}</Table.Cell>
         <Table.Cell>
           <Link to={`/edit/${this.props.stuff._id}`}>Edit</Link>
+        </Table.Cell>
+        <Table.Cell>
+          <Button icon>
+            <Icon name = 'trash' />
+          </Button>
         </Table.Cell>
       </Table.Row>
     );
